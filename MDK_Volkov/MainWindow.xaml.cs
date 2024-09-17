9aac884f233e9f9e8a095c4d5695a731462ea6c0
Aleksei01111 <alekseivolkov01012@gmail.com>
2024-09-17 09:27:20 +0300

    Add project files.

---
+++ MDK_Volkov/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace MDK_Volkov
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            if (LoginTxb.Text == "admin" && PasswordPsb.Password == "admin")
+                MessageBox.Show("Введены верные данные", "Пока", MessageBoxButton.OK, MessageBoxImage.Information);
+            else
+                MessageBox.Show("Введены неверные данные", "ОШИБКА!!!", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            new WinRegister().Show();
+            Close();
+        }
+    }
+}
